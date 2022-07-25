bcdfd9c15e4859728d363c8bbf5432b7e04707c5
AxelVTN <axelvatan@orange.fr>
2022-07-25 02:13:36 +0200

    Added basic auth system.

--- core/src/main/java/fr/AxelVatan/CMWLink/Common/WebServer/WebServer.java
+++ core/src/main/java/fr/AxelVatan/CMWLink/Common/WebServer/WebServer.java
@@ -1,12 +1,19 @@
 package fr.AxelVatan.CMWLink.Common.WebServer;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.logging.Level;
+
+import org.mindrot.jbcrypt.BCrypt;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -22,6 +29,27 @@ public class WebServer {
 	private Express app;
 	private @Getter HashMap<String, IRoute> routes;
 	
+	//TEST CODE
+	public static void main(String a[]){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://127.0.0.1:24102/boutique/version"))
+                .GET()
+                .header("User", "admin")
+                .header("Pwd", "changme")
+                .header("Content-Type", "application/json")
+                .build();
+        HttpResponse<String> response = null;
+		try {
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        System.out.println(response.body());
+    }
+	//
+	
 	public WebServer(ConfigFile config) {
 		this.config = config;
 		this.app = new Express();
@@ -35,8 +63,21 @@ public class WebServer {
 		});
 		authRequest();
 	}
-
+	
 	private void authRequest() {
+		app.use((req, res) -> {
+			String user = req.getHeader("User").get(0);
+			String pwd = req.getHeader("Pwd").get(0);
+			String userAndPwdFromHost = BCrypt.hashpw(user + ":" + pwd, BCrypt.gensalt(10));
+			Boolean match = BCrypt.checkpw(config.getConfig().getUsername() + ":" + config.getConfig().getPassword(), userAndPwdFromHost);
+			if(!match) {
+				JsonObject jsObj = new JsonObject();
+				jsObj.addProperty("CODE", 401);
+				jsObj.addProperty("MESSAGE", "User not authorized to execute this request.");
+				config.getLog().severe("User: " + user + " from host: " + req.getIp() + " is not authorized to execute the route: " + req.getPath());
+				res.send(jsObj.toString());
+			}
+		});
 	}
 	
 	public void startWebServer() {
@@ -98,6 +139,10 @@ public class WebServer {
 			}
 		}
 		routes.put("/", null);
+		handleNonExistingRoutes();
+	}
+
+	private void handleNonExistingRoutes() {
 		app.all((req, res) -> {
 			JsonObject jsObj = new JsonObject();
 			jsObj.addProperty("CODE", 404);
@@ -105,7 +150,7 @@ public class WebServer {
 			res.send(jsObj.toString());
 		});
 	}
-
+	
 	public void disable() {
 		this.app.stop();
 	}
