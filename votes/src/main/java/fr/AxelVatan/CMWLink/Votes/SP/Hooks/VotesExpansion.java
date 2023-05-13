0043e09827df9ba8c6d0fee5fd67e7273629d4f5
Teyir <60007549+Teyir@users.noreply.github.com>
2023-05-13 16:38:53 +0200

    [IMPROVE] Start VotesExpansion

---
+++ votes/src/main/java/fr/AxelVatan/CMWLink/Votes/SP/Hooks/VotesExpansion.java
@@ -0,0 +1,36 @@
+package fr.AxelVatan.CMWLink.Votes.SP.Hooks;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class VotesExpansion extends PlaceholderExpansion {
+    @Override
+    public @NotNull String getName() {
+        return "CMWLink - Votes";
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "cmwl";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "CraftMyWebsite / Teyir";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0";
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, @NotNull String params) {
+
+        if (params.equals("votes_total_votes")){
+            return "a";
+        }
+        return null;
+    }
+}
